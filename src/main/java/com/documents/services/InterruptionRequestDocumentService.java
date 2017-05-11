b3e79c46d97d6cb8e69e83c7fef8dc70eb45c698
ssimona01 <simona_stolnicu@yahoo.com>
2017-05-11 17:54:27 +0300

    - updated InterruptionRequestDocumentRepository - created InterruptionRequestDocumentService interface - created InterruptionRequestDocumentServiceImpl

---
+++ src/main/java/com/documents/services/InterruptionRequestDocumentService.java
@@ -0,0 +1,9 @@
+package com.documents.services;
+
+import com.documents.models.InterruptionRequestDocument;
+
+/**
+ * Created by Simona on 11-May-17.
+ */
+public interface InterruptionRequestDocumentService extends CrudService<InterruptionRequestDocument> {
+}
