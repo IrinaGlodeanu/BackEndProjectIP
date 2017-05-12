e658c8c03fce94116ed9ce8babcc86d9fb8aeaa7
IrinaGlodeanu <irina_glodeanu96@yahoo.com>
2017-05-12 20:24:33 +0300

    -created controllers for Transportation Request Document  -implemented methods

--- src/main/java/com/documents/controllers/InterruptionRequestDocumentController.java
+++ src/main/java/com/documents/controllers/InterruptionRequestDocumentController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +15,9 @@ import com.documents.models.InterruptionRequestDocument;
 import com.documents.services.InterruptionRequestDocumentService;
 
 /**
- * Created by AlenaHa on 12.05.2017.
+ * Created by AlenaHa && Irinaaaa on 12.05.2017.
  */
+
 @RestController
 @RequestMapping(value = "/interruption")
 public class InterruptionRequestDocumentController {
@@ -39,4 +41,34 @@ public class InterruptionRequestDocumentController {
         List<InterruptionRequestDocument> interruptionDocumentList = interruptionService.findAll();
         return new ResponseEntity<List<InterruptionRequestDocument>>(interruptionDocumentList, HttpStatus.OK);
     }
+
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<InterruptionRequestDocument> deleteInterruptionRequest(@PathVariable Long id) {
+        InterruptionRequestDocument requestDocument = interruptionService.findById(id);
+        if (requestDocument != null) {
+            interruptionService.delete(id);
+        } else {
+            return new ResponseEntity<InterruptionRequestDocument>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<InterruptionRequestDocument>(HttpStatus.OK);
+    }
+
+
+    @RequestMapping(value = "/interruption", method = RequestMethod.POST)
+    public ResponseEntity<InterruptionRequestDocument> createInterruptionRequest(@RequestBody InterruptionRequestDocument newStudent) {
+        InterruptionRequestDocument savedRequestDocument = interruptionService.save(newStudent);
+        return new ResponseEntity<InterruptionRequestDocument>(savedRequestDocument, HttpStatus.CREATED);
+    }
+
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<InterruptionRequestDocument> updateInterruptionRequest(@RequestBody InterruptionRequestDocument interruptionRequest, @PathVariable Long id) {
+        if (!id.equals(interruptionRequest.getId())) {
+            return new ResponseEntity<InterruptionRequestDocument>(HttpStatus.BAD_REQUEST);
+        }
+        InterruptionRequestDocument newStudent = interruptionService.save(interruptionRequest);
+        return new ResponseEntity<InterruptionRequestDocument>(newStudent, HttpStatus.OK);
+    }
 }
+
--- src/main/java/com/documents/controllers/TransportRequestDocumentController.java
+++ src/main/java/com/documents/controllers/TransportRequestDocumentController.java
@@ -1,4 +1,70 @@
 package com.documents.controllers;
 
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.documents.models.TransportRequestDocument;
+import com.documents.services.TransportRequestDocumentService;
+
+@RestController
+@RequestMapping(value = "/transport")
 public class TransportRequestDocumentController {
+
+    @Autowired
+    TransportRequestDocumentService transportRequestDocumentService;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<TransportRequestDocument> getRequestbyId(@PathVariable Long id) {
+        TransportRequestDocument transportDocument = transportRequestDocumentService.findById(id);
+        if (transportDocument != null) {
+            return new ResponseEntity<TransportRequestDocument>(transportDocument, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<TransportRequestDocument>(HttpStatus.NO_CONTENT);
+        }
+    }
+
+
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public ResponseEntity<List<TransportRequestDocument>> getAllInterruptionRequest() {
+        List<TransportRequestDocument> interruptionDocumentList = transportRequestDocumentService.findAll();
+        return new ResponseEntity<List<TransportRequestDocument>>(interruptionDocumentList, HttpStatus.OK);
+    }
+
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<TransportRequestDocument> deleteInterruptionRequest(@PathVariable Long id) {
+        TransportRequestDocument requestDocument = transportRequestDocumentService.findById(id);
+        if (requestDocument != null) {
+            transportRequestDocumentService.delete(id);
+        } else {
+            return new ResponseEntity<TransportRequestDocument>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<TransportRequestDocument>(HttpStatus.OK);
+    }
+
+
+    @RequestMapping(value = "/transportation", method = RequestMethod.POST)
+    public ResponseEntity<TransportRequestDocument> createInterruptionRequest(@RequestBody TransportRequestDocument newStudent) {
+        TransportRequestDocument savedRequestDocument = transportRequestDocumentService.save(newStudent);
+        return new ResponseEntity<TransportRequestDocument>(savedRequestDocument, HttpStatus.CREATED);
+    }
+
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<TransportRequestDocument> updateInterruptionRequest(@RequestBody TransportRequestDocument interruptionRequest, @PathVariable Long id) {
+        if (!id.equals(interruptionRequest.getId())) {
+            return new ResponseEntity<TransportRequestDocument>(HttpStatus.BAD_REQUEST);
+        }
+        TransportRequestDocument newStudent = transportRequestDocumentService.save(interruptionRequest);
+        return new ResponseEntity<TransportRequestDocument>(newStudent, HttpStatus.OK);
+    }
 }
