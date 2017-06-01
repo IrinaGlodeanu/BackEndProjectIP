70ae585c74a2945ec8e73ddf86da7f9e889cb37d
IrinaGlodeanu <irina_glodeanu96@yahoo.com>
2017-06-01 17:23:04 +0300

    -reimplemented tests following the Arrange->Act->Assert pattern -implemented tests for findAll method -segregated concerns following the SoC design principle (behaviour and functionality) -major refactoring

--- src/test/java/com/documents/LicenseRegistrationTests.java
+++ src/test/java/com/documents/LicenseRegistrationTests.java
@@ -1,8 +1,15 @@
 package com.documents;
 
-import com.documents.models.LicenseRegistrationForm;
-import com.documents.repositories.LicenseRegistrationFormRepository;
-import com.documents.services.LicenseRegistrationFormServiceImpl;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,15 +18,15 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import com.documents.models.LicenseRegistrationForm;
+import com.documents.repositories.LicenseRegistrationFormRepository;
+import com.documents.services.LicenseRegistrationFormServiceImpl;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LicenseRegistrationTests {
 
+
+    //Arrange
     @Mock
     private LicenseRegistrationFormRepository licenseRegistrationFormRepository;
 
@@ -31,78 +38,185 @@ public class LicenseRegistrationTests {
         MockitoAnnotations.initMocks(this);
     }
 
+
+    public void setup(LicenseRegistrationForm licenseRegistrationFormToSet) {
+
+        licenseRegistrationFormToSet.setId((long) 3);
+        licenseRegistrationFormToSet.setDocumentName("Document_Name");
+        licenseRegistrationFormToSet.setStudentName("Student_Name");
+        licenseRegistrationFormToSet.setMantricolNumber("123412123123");
+        licenseRegistrationFormToSet.setGraduationYear(2018);
+        licenseRegistrationFormToSet.setDateReceiptLinguisticCompetence("12-June-2015");
+        licenseRegistrationFormToSet.setPaymentReceiptLinguisticCompetence(300);
+        licenseRegistrationFormToSet.setDateReceiptRedoExam("14-August-2016");
+        licenseRegistrationFormToSet.setPaymentReceiptRedoExam(300);
+
+    }
+
     @Test
-    public void licenseRequest_save_should_return_true() throws Exception {
+    public void behavioural_licenseRequest_save_should_return_true() throws Exception {
+
+        //Act
+        LicenseRegistrationForm licenseRegistrationFormAfterSave = new LicenseRegistrationForm();
         LicenseRegistrationForm licenseRegistrationFormToSave = new LicenseRegistrationForm();
-        Long id = (long) 3;
-        String documentName = "Document_Name";
-        String studentName = "Student_Name";
-        String nrMatricol = "123412123123";
-        Integer graduationYear = 2018;
-        String dateReceiptLinguisticCompetence = "12-June-2015";
-        Integer paymentReceiptLinguisticCompetence = 300;
-        String dateReceiptRedoExam = "14-August-2016";
-        Integer paymentReceiptRedoExam = 300;
+
+        setup(licenseRegistrationFormToSave);
+
+        when(licenseRegistrationFormRepository.save(any(LicenseRegistrationForm.class))).thenReturn(licenseRegistrationFormAfterSave);
+
+        licenseRegistrationFormAfterSave = licenseRegistrationFormServiceImpl.save(licenseRegistrationFormToSave);
+
+        //Assert
+        assertNotNull(licenseRegistrationFormAfterSave);
+    }
+
+    @Test
+    public void functionality_licenseRequest_save_should_return_licenseRequest() throws Exception {
+
+        //Act
         LicenseRegistrationForm licenseRegistrationFormAfterSave = new LicenseRegistrationForm();
 
-        licenseRegistrationFormAfterSave.setId(id);
-        licenseRegistrationFormAfterSave.setDocumentName(documentName);
-        licenseRegistrationFormAfterSave.setStudentName(studentName);
-        licenseRegistrationFormAfterSave.setMantricolNumber(nrMatricol);
-        licenseRegistrationFormAfterSave.setGraduationYear(graduationYear);
-        licenseRegistrationFormAfterSave.setDateReceiptLinguisticCompetence(dateReceiptLinguisticCompetence);
-        licenseRegistrationFormAfterSave.setPaymentReceiptLinguisticCompetence(paymentReceiptLinguisticCompetence);
-        licenseRegistrationFormAfterSave.setDateReceiptRedoExam(dateReceiptRedoExam);
-        licenseRegistrationFormAfterSave.setPaymentReceiptRedoExam(paymentReceiptRedoExam);
+        setup(licenseRegistrationFormAfterSave);
 
-        when(licenseRegistrationFormServiceImpl.save(any(LicenseRegistrationForm.class))).thenReturn(licenseRegistrationFormAfterSave);
+        when(licenseRegistrationFormRepository.save(any(LicenseRegistrationForm.class))).thenReturn(licenseRegistrationFormAfterSave);
 
-        LicenseRegistrationForm newLicenseRegistration = licenseRegistrationFormServiceImpl.save(licenseRegistrationFormToSave);
-        assertNotNull(newLicenseRegistration);
-        assertEquals(id, newLicenseRegistration.getId());
-        assertEquals(documentName, newLicenseRegistration.getDocumentName());
-        assertEquals(studentName, newLicenseRegistration.getStudentName());
-        assertEquals(nrMatricol, newLicenseRegistration.getMantricolNumber());
-        assertEquals(graduationYear, newLicenseRegistration.getGraduationYear());
-        assertEquals(dateReceiptLinguisticCompetence, newLicenseRegistration.getDateReceiptLinguisticCompetence());
-        assertEquals(paymentReceiptLinguisticCompetence, newLicenseRegistration.getPaymentReceiptLinguisticCompetence());
-        assertEquals(dateReceiptRedoExam, newLicenseRegistration.getDateReceiptRedoExam());
-        assertEquals(paymentReceiptRedoExam, newLicenseRegistration.getPaymentReceiptRedoExam());
+        LicenseRegistrationForm savedLicenseRegForm = licenseRegistrationFormServiceImpl.save(licenseRegistrationFormAfterSave);
 
+        //Assert
+        assertEquals(Long.valueOf(3), savedLicenseRegForm.getId());
+        assertEquals("Document_Name", savedLicenseRegForm.getDocumentName());
+        assertEquals("Student_Name", savedLicenseRegForm.getStudentName());
+        assertEquals("123412123123", savedLicenseRegForm.getMantricolNumber());
+        assertEquals(Integer.valueOf(2018), savedLicenseRegForm.getGraduationYear());
+        assertEquals("12-June-2015", savedLicenseRegForm.getDateReceiptLinguisticCompetence());
+        assertEquals(Integer.valueOf(300), savedLicenseRegForm.getPaymentReceiptLinguisticCompetence());
+        assertEquals("14-August-2016", savedLicenseRegForm.getDateReceiptRedoExam());
+        assertEquals(Integer.valueOf(300), savedLicenseRegForm.getPaymentReceiptRedoExam());
 
     }
 
     @Test
-    public void licenseRequest_findById_should_return_true() throws Exception {
+    public void behavioural_licenseRequest_findById_should_return_true() throws Exception {
+
+        //Act
         LicenseRegistrationForm licenseRegistrationFormToSave = new LicenseRegistrationForm();
-        long userId = (long) 123;
-        licenseRegistrationFormToSave.setId(userId);
-        when(licenseRegistrationFormServiceImpl.findById(any(long.class))).thenReturn(licenseRegistrationFormToSave);
-        LicenseRegistrationForm newLicenseRegistration = licenseRegistrationFormServiceImpl.findById(licenseRegistrationFormToSave.getId());
-        long userIdAfter = newLicenseRegistration.getId();
-        assertNotNull(newLicenseRegistration);
-        assertEquals(userId, userIdAfter);
+
+        when(licenseRegistrationFormRepository.findOne(any(long.class))).thenReturn(licenseRegistrationFormToSave);
+
+        licenseRegistrationFormToSave = licenseRegistrationFormServiceImpl.findById((long) 123);
+
+        //Assert
+        assertNotNull(licenseRegistrationFormToSave);
     }
 
     @Test
-    public void licenseRequest_deleteById_should_return_true() throws Exception {
+    public void functionality_licenseRequest_findById_should_return_licenseRequest() throws Exception {
+
+        //Act
+        LicenseRegistrationForm licenseRegistrationFormToFind = new LicenseRegistrationForm();
+
+        setup(licenseRegistrationFormToFind);
+
+        when(licenseRegistrationFormRepository.findOne(any(long.class))).thenReturn(licenseRegistrationFormToFind);
+
+        LicenseRegistrationForm findedLicenseRegForm = licenseRegistrationFormServiceImpl.findById((long) 3);
+
+        //Assert
+        assertEquals(Long.valueOf(3), findedLicenseRegForm.getId());
+        assertEquals("Document_Name", findedLicenseRegForm.getDocumentName());
+        assertEquals("Student_Name", findedLicenseRegForm.getStudentName());
+        assertEquals("123412123123", findedLicenseRegForm.getMantricolNumber());
+        assertEquals(Integer.valueOf(2018), findedLicenseRegForm.getGraduationYear());
+        assertEquals("12-June-2015", findedLicenseRegForm.getDateReceiptLinguisticCompetence());
+        assertEquals(Integer.valueOf(300), findedLicenseRegForm.getPaymentReceiptLinguisticCompetence());
+        assertEquals("14-August-2016", findedLicenseRegForm.getDateReceiptRedoExam());
+        assertEquals(Integer.valueOf(300), findedLicenseRegForm.getPaymentReceiptRedoExam());
+
+    }
+
+    @Test
+    public void behavioural_licenseRequest_deleteById_should_return_true() throws Exception {
+
+        //Act
         LicenseRegistrationForm licenseRegistrationForm = new LicenseRegistrationForm();
-        long userId = 3;
-        licenseRegistrationForm.setId(userId);
+
+        licenseRegistrationForm.setId((long) 3);
+
         licenseRegistrationFormServiceImpl.delete(licenseRegistrationForm.getId());
+
+        LicenseRegistrationForm findedLicenseRegistrationForm = licenseRegistrationFormServiceImpl.findById((long) 3);
+
+        //Assert
+        assertNull(findedLicenseRegistrationForm);
+
+    }
+
+
+    @Test
+    public void functionality_licenseRequest_deleteById_should_return_delete_licenseRequest() throws Exception {
+
+        //Act
+        LicenseRegistrationForm licenseRegistrationForm = new LicenseRegistrationForm();
+
+        licenseRegistrationFormServiceImpl.delete(licenseRegistrationForm.getId());
+
+        //Assert
         verify(licenseRegistrationFormRepository).delete(licenseRegistrationForm.getId());
 
     }
 
-   /* @Test
-    public void licenseRequest_findAll_should_return_true() throws Exception {
-        List<LicenseRegistrationForm> forms = new ArrayList<>();
-        List<LicenseRegistrationForm> frm = new ArrayList<>();
+    @Test
+    public void behavioural_licenseRequest_findAll_should_return_true() throws Exception {
+
+        //Act
+        List<LicenseRegistrationForm> licenseRegistrationForms = new ArrayList<>();
+
         LicenseRegistrationForm licenseRegistrationForm = new LicenseRegistrationForm();
-        forms.add(licenseRegistrationForm);
-        Mockito.when(licenseRegistrationFormServiceImpl.findAll()).thenReturn(forms);
-        frm = licenseRegistrationFormServiceImpl.findAll();
-        assertNotNull(frm);
+
+        setup(licenseRegistrationForm);
+
+        when(licenseRegistrationFormRepository.findAll()).thenReturn(licenseRegistrationForms);
+
+        licenseRegistrationForms.add(licenseRegistrationForm);
+
+        List<LicenseRegistrationForm> findedForms;
+        findedForms = licenseRegistrationFormServiceImpl.findAll();
+
+        //Assert
+        assertNotNull(findedForms);
+
     }
-*/
+
+    @Test
+    public void functionality_licenseRequest_findAll_should_return_list_of_licenseRequest() throws Exception {
+
+        //Act
+        List<LicenseRegistrationForm> licenseRegistrationForms = new ArrayList<>();
+
+        LicenseRegistrationForm licenseRegistrationForm = new LicenseRegistrationForm();
+
+        setup(licenseRegistrationForm);
+
+        when(licenseRegistrationFormRepository.findAll()).thenReturn(licenseRegistrationForms);
+
+        licenseRegistrationForms.add(licenseRegistrationForm);
+
+        List<LicenseRegistrationForm> findedForms;
+        findedForms = licenseRegistrationFormServiceImpl.findAll();
+
+        //Assert
+        assertEquals(licenseRegistrationForms.get(0).getId(), findedForms.get(0).getId());
+        assertEquals(licenseRegistrationForms.get(0).getDocumentName(), findedForms.get(0).getDocumentName());
+        assertEquals(licenseRegistrationForms.get(0).getStudentName(), findedForms.get(0).getStudentName());
+        assertEquals(licenseRegistrationForms.get(0).getMantricolNumber(), findedForms.get(0).getMantricolNumber());
+        assertEquals(licenseRegistrationForms.get(0).getGraduationYear(), findedForms.get(0).getGraduationYear());
+        assertEquals(licenseRegistrationForms.get(0).getDateReceiptLinguisticCompetence(), findedForms.get(0).getDateReceiptLinguisticCompetence());
+        assertEquals(licenseRegistrationForms.get(0).getPaymentReceiptLinguisticCompetence(), findedForms.get(0).getPaymentReceiptLinguisticCompetence());
+        assertEquals(licenseRegistrationForms.get(0).getDateReceiptRedoExam(), findedForms.get(0).getDateReceiptRedoExam());
+        assertEquals(licenseRegistrationForms.get(0).getPaymentReceiptRedoExam(), findedForms.get(0).getPaymentReceiptRedoExam());
+
+    }
+
 }
+
+
